f4af52c3f835b218d2726a3c4cf3996a0587c766
wenhaobi <wenhaobi1999@hotmail.com>
2021-03-10 17:50:21 -0500

    added origin for cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 
 
 //Middlewares
-app.use(cors({ credentials: true }));
+app.use(cors({ credentials: true, origin: "http://localhost:3001/" }));
 app.use(express.json());
 app.use(cookieParser());
 
