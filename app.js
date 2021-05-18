adb0180b49895c7a8f19abf1c2e596eb226de859
wenhaobi <wenhaobi1999@hotmail.com>
2021-05-17 22:28:22 -0400

    fixed env var call

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 
 
 //Middlewares
-app.use(cors({ credentials: true, origin: REACT_APP_CORS_LINK }));
+app.use(cors({ credentials: true, origin: process.env.REACT_APP_CORS_LINK }));
 app.use(express.json());
 app.use(cookieParser());
 
